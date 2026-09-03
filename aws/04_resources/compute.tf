45939d4fed5ae91d69743dbcf342f95392a2959c
abhinav14mishra <123477417+abhinav14mishra@users.noreply.github.com>
2026-09-03 03:17:25 +0000

    Add Terraform configuration files for AWS infrastructure setup including VPC, subnets, security groups, and EC2 instance

---
+++ aws/04_resources/compute.tf
@@ -0,0 +1,27 @@
+
+resource "aws_instance" "web" {
+  ami           = "ami-090d68841c2a28756"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp3"
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              dnf update -y
+              dnf install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              echo "<h1>Deployed on Amazon Linux 2023 via Terraform</h1>" > /usr/share/nginx/html/index.html
+              EOF
+  tags = merge(local.common_tags, {
+    Name = "web_instance"
+  })
+
+}
+
+output "web_instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
---
+++ aws/04_resources/networking.tf
@@ -0,0 +1,47 @@
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+  tags = merge(local.common_tags, {
+    Name = "vpc"
+  })
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  tags = merge(local.common_tags, {
+    Name = "public_subnet"
+  })
+}
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.0.1.0/24"
+  tags = merge(local.common_tags, {
+    Name = "private_subnet"
+  })
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+  tags = merge(local.common_tags, {
+    Name = "internet_gateway"
+  })
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = merge(local.common_tags, {
+    Name = "route_table"
+  })
+
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.route_table.id
+}
